da2d31ab55a7ac89fc784da26ef49b6ced188be6
kammarx <kammarufa6@gmail.com>
2022-01-13 13:17:22 +0500

    Добавьте файлы проекта.

---
+++ laba05proga/laba05proga.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Rectangle {
+
+private:
+	int square;
+protected:
+	int coordX, coordY;
+public:
+	Rectangle() {
+		cout << "Rectangle() - default constructor is called\n";
+		coordX = 1;
+		coordY = 1;
+	}
+
+	Rectangle(int coordX, int coordY) {
+		cout << "Rectangle() - constructor with arguments is called\n";
+		this->coordX = coordX;
+		this->coordY = coordY;
+
+	}
+
+	Rectangle(const Rectangle& rec) {
+		this->coordX = rec.coordX;
+		this->coordY = rec.coordY;
+	}
+};
+
+
+
+int main(){
+
+
+	return 0;
+}
